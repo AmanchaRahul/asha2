604073bb4d69daee19f094fad42a5c50a7760dac
Amancharahul <amancharahul25@gmail.com>
2024-11-12 14:08:37 +0530

    changes made

--- ashapro1/settings.py
+++ ashapro1/settings.py
@@ -135,8 +135,12 @@ WSGI_APPLICATION = 'ashapro1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.byfrbfbdvwgetvzvczpa',
+        'PASSWORD': 'Ramgopalvarma@3',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',  # Change this to your Supabase host
+        'PORT': '6543',
     }
 }
 
