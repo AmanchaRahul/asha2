66c0d898671dfb1fd5cda3ca599c87d3a048b940
Amancharahul <amancharahul25@gmail.com>
2024-11-12 15:09:21 +0530

    changes made

--- ashapro1/settings.py
+++ ashapro1/settings.py
@@ -130,17 +130,13 @@ WSGI_APPLICATION = 'ashapro1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.byfrbfbdvwgetvzvczpa',
-        'PASSWORD': 'Ramgopalvarma@3',
-        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',  # Change this to your Supabase host
-        'PORT': '6543',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
