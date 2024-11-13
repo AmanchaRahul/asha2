02a2414029136c16efa8a95348edc7c521699ca7
Amancharahul <amancharahul25@gmail.com>
2024-11-13 13:39:53 +0530

    changes made

--- ashapro1/settings.py
+++ ashapro1/settings.py
@@ -130,17 +130,19 @@ WSGI_APPLICATION = 'ashapro1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
+}
 
 
 # settings.py
 
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -152,7 +154,7 @@ DATABASES = {
     }
 }
 
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
