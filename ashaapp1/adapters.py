3c980a5711fe7f9446d7b5ee4071c83aa866fdae
Amancharahul <amancharahul25@gmail.com>
2024-11-13 18:05:07 +0530

    changes made

--- ashaapp1/adapters.py
+++ ashaapp1/adapters.py
@@ -1,6 +1,25 @@
+# adapters.py
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.account.utils import user_email_at_domain
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_auto_signup_allowed(self, request, sociallogin):
-        # Skip the intermediate page and allow auto signup
-        return True
+        # Allow auto signup for Google users
+        return True  # Allow signup automatically if the user doesn't exist
+
+    def pre_social_login(self, request, sociallogin):
+        """
+        This is called before the login process.
+        We can use it to customize the login/signup behavior.
+        """
+        user = sociallogin.user
+
+        # Check if the user already exists by email and log them in directly if they do
+        if user_email_at_domain(user.email):
+            try:
+                existing_user = self.get_account(user.email)
+                sociallogin.user = existing_user
+            except User.DoesNotExist:
+                pass  # If user doesn't exist, it'll go through the signup process
+        return sociallogin
+
