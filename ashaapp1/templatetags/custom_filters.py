28d766ff2053d18792f83327e5b054032a2a1f59
Amancharahul <amancharahul25@gmail.com>
2024-11-19 18:52:39 +0530

    changes made

--- ashaapp1/templatetags/custom_filters.py
+++ ashaapp1/templatetags/custom_filters.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter
+def subtract(value, arg):
+    return value - arg
